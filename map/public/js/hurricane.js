c5983c88535dac20fa5945e3507fc3e1c16bdb5b
Bret Jones <45405964+Iceman013@users.noreply.github.com>
2024-01-23 09:04:34 -0500

    Temporary hurricane display

--- map/public/js/hurricane.js
+++ map/public/js/hurricane.js
@@ -1,4 +1,19 @@
 function drawHurricane() {
-    omnivore.kml('AL112017_020adv_CONE.kml').addTo(map);
-    omnivore.kml('al112017_020adv_TRACK.kml').addTo(map);
+    /*
+    let url = "https://www.nhc.noaa.gov/gis/kml/nhc.kmz";
+    let response = await fetch(url, {
+        mode: "cors"
+    });
+    console.log(response)
+    */
+    // omnivore.kml('https://iblogbox.github.io/js/gpx/sample/cornishlight.kml').addTo(map);
+    // fetch('al102023_029adv_CONE.kml')
+    // .then((res) => res.text()).then((text) => console.log(text));
+    omnivore.kml('al102023_029adv_CONE.kml').addTo(map);
+    omnivore.kml('al102023_029adv_TRACK.kml').addTo(map);
+
+    // var kmz = L.kmzLayer().addTo(map);
+
+    // kmz.load('regions.kmz')
+    // kmz.load('AL102023_029adv_CONE.kmz');
 }
