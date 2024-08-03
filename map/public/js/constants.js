d9e36e8ab9c7f018d087f60fa45c5435e7dbd939
Bret Jones <45405964+Iceman013@users.noreply.github.com>
2024-08-03 03:47:47 -0400

    Add tropical storm chris

--- map/public/js/constants.js
+++ map/public/js/constants.js
@@ -3,5 +3,5 @@ const DISPLAY_TO_URL = [
     ["Daily Forecast", "forecast"],
     ["Hurricane", "Hurricane"]
 ];
-const HURRICANES = ["Beryl","Idalia","Nicole","Ian"];
+const HURRICANES = ["Beryl","Chris","Idalia","Nicole","Ian"];
 const VERSION = "1.0.7";
