6d262aa9f11596a0eba194eab94b451fdc4b32f5
Bret Jones <45405964+Iceman013@users.noreply.github.com>
2024-09-25 15:35:39 -0400

    Remove useless files

--- map/public/js/draw.js
+++ map/public/js/draw.js
@@ -31,7 +31,6 @@ function addTifLayers() {
             return button;
         }
     })
-    map.addControl(new fullscreen());
     let promiseList = [];
 
     console.log(tiffList);
@@ -88,98 +87,4 @@ function addTifLayers() {
         rasterize(url_to_geotiff_file);
     }
     return promiseList;
-}
-
-function sortBy(sort) {
-    for (let i = 0; i < overLayers.length - 1; i++) {
-        for (let j = i + 1; j < overLayers.length; j++) {
-            if (sort(overLayers[i], overLayers[j]) > 0) {
-                var temp = overLayers[i];
-                overLayers[i] = overLayers[j];
-                overLayers[j] = temp;
-            }
-        }
-    }
-}
-// Creates the control panel for layer display
-function makeControl() {
-    var sort;
-    var SortType = document.getElementById("SortType").value;
-    if (SortType == "Alphabetical") {
-        sort = function(inputa, inputb) {
-            var itema = inputa.tiff.name;
-            var itemb = inputb.tiff.name;
-            return itema.localeCompare(itemb);
-        }
-    } else if (SortType == "Date") {
-        sort = function(inputa, inputb) {
-            var itema = inputa.tiff.description;
-            var itemb = inputb.tiff.description;
-            return itemb.localeCompare(itema);
-        }
-    }
-
-    sortBy(sort);
-    var activeLayers = document.getElementsByName("Overlayer");
-    for (let i = 0; i < activeLayers.length; i++) {
-        activeLayers[i].dispatchEvent(new Event("userChange"));
-    }
-    var base = document.getElementById("overLayers");
-    while (base.firstChild) {
-        base.removeChild(base.firstChild);
-    }
-
-    // Get mode type to display
-    let ModelType = "None";
-    var TModelType = document.getElementById("ModelType").value;
-    for (let i = 0; i < DISPLAY_TO_URL.length; i++) {
-        if (DISPLAY_TO_URL[i][0] == TModelType) {
-            ModelType = DISPLAY_TO_URL[i][1];
-        }
-    }
-
-    // Hurricane
-    if (ModelType == "Hurricane") {
-        for (let i = 0; i < HURRICANES.length; i++) {
-            let thisH = document.createElement("div");
-            thisH.id = HURRICANES[i];
-
-            let title = document.createElement("h3");
-            title.innerText = HURRICANES[i];
-            thisH.appendChild(title);
-
-            base.appendChild(thisH);
-        }
-    }
-
-    // Add the radio button selectors
-    for (let i = 0; i < overLayers.length; i++) {
-        if (overLayers[i].tiff.type == ModelType) {
-            if (ModelType == "forecast") {
-                var element = makeOverLayerControl(overLayers[i]);
-                base.appendChild(element);
-            } else if (ModelType == "Hurricane") {
-                var element = makeOverLayerControl(overLayers[i]);
-                document.getElementById(overLayers[i].tiff.hurricane).appendChild(element);
-            }
-        }
-    }
-
-    // Open top tiff
-    let hurricanes = document.getElementsByClassName("overlayer-button-here");
-    if (hurricanes.length > 0) {
-        hurricanes[0].dispatchEvent(new Event("change"));
-    }
-}
-
-// Draws all elements
-function drawLayers() {
-    // Find the map
-    // This is a pain. Don't get me started
-    map = mapsPlaceholder[0];
-    // Make the Tif layers
-    // Then add the control sidebar
-    Promise.all(addTifLayers()).then(makeControl);
-    document.getElementById("ModelType").addEventListener("change", makeControl);
-    document.getElementById("SortType").addEventListener("change", makeControl);
 }
